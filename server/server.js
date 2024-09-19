6e0c3ad2d7d4d2add9433cb28c6903a1f47550fe
iHaveNoKnowledge <hackdk12@gmail.com>
2024-09-19 15:24:03 +0700

    initial server for test

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require("path");
+const winston = require("winston");
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res, next) => {
+    res.send("Welcum")
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log("server start at port: " + port);
+});
